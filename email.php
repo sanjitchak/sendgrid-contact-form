4099daa169f157778cfb13e5b00339d71ea00b02
Sanjit <sanjit@sanjitchakrabarti.com>
2020-12-16 01:44:16 +0530

    first commit

--- email.php
+++ email.php
@@ -12,7 +12,7 @@ $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$sendgrid = new \SendGrid(getenv('API'));
+$sendgrid = new \SendGrid(getenv('API_KEY'));
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
